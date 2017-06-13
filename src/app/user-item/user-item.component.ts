5719b1ea53f21e6d014e02eba7c0ca03bb90f853
iman <iman@bitmascot.com>
2017-06-13 14:58:27 +0600

    user list

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent implements OnInit {
 
-  name:string;
+  @Input() name:string;
 
   constructor() { }
 
   ngOnInit() {
-    this.name ='Felipe';
+    //this.name ='Felipe';
   }
 
 }
